9b26f2f0e945b6e4c87ea4ed1a5654e57e67f5f3
HalfSilver <69383504+Half-Silver@users.noreply.github.com>
2026-08-27 16:06:16 +0000

    fix(script): Update redis-cli flush command to use

--- src/nextcloud/fixes/existing-install/08_flush_redis_db.sh
+++ src/nextcloud/fixes/existing-install/08_flush_redis_db.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# shellcheck source=src/redis/utilities/redis-utilities
+. "$SNAP/utilities/redis-utilities"
+
 # This is required by richdocuments so that it does not break installations.
 # See https://github.com/nextcloud/richdocuments/issues/3780#issuecomment-2257677440
-"$SNAP"/bin/redis-cli -s /tmp/sockets/redis.sock FLUSHDB
+"$SNAP"/bin/redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" FLUSHDB
