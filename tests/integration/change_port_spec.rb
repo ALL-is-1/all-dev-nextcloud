e3b5bc5afe449b8117a64277a976898fc3cc614b
HalfSilver <69383504+Half-Silver@users.noreply.github.com>
2026-08-27 16:07:44 +0000

    test(integration): Update port expectations in cha

--- tests/integration/change_port_spec.rb
+++ tests/integration/change_port_spec.rb
@@ -1,27 +1,27 @@
 feature "Change ports" do
 	feature "http" do
 		after(:all) do
-			set_config "ports.http": 80, "ports.https": 443
+			set_config "ports.http": DEFAULT_HTTP_PORT, "ports.https": DEFAULT_HTTPS_PORT
 			wait_for_nextcloud
 		end
 
 		scenario "http" do
-			set_config "ports.http": 81
+			set_config "ports.http": 21803
 			expect($?.to_i).to eq 0
-			wait_for_nextcloud(port: 81)
-			Capybara.app_host = 'http://localhost:81'
+			wait_for_nextcloud(port: 21803)
+			Capybara.app_host = 'http://localhost:21803'
 
 			visit "/"
-			assert_uri(https: false, port: 81)
+			assert_uri(https: false, port: 21803)
 
 			# Also assert that we can change it back to the default
-			set_config "ports.http": 80
+			set_config "ports.http": DEFAULT_HTTP_PORT
 			expect($?.to_i).to eq 0
 			wait_for_nextcloud
-			Capybara.app_host = 'http://localhost'
+			Capybara.app_host = DEFAULT_HTTP_HOST
 
 			visit "/"
-			assert_uri(https: false, port: 80)
+			assert_uri(https: false, port: DEFAULT_HTTP_PORT)
 		end
 	end
 
@@ -31,59 +31,59 @@ feature "Change ports" do
 		end
 
 		after(:all) do
-			set_config "ports.http": 80, "ports.https": 443
+			set_config "ports.http": DEFAULT_HTTP_PORT, "ports.https": DEFAULT_HTTPS_PORT
 			wait_for_nextcloud
 			disable_https
 		end
 
 		scenario "https" do
-			set_config "ports.https": 444
+			set_config "ports.https": 15136
 			expect($?.to_i).to eq 0
-			wait_for_nextcloud(https: true, port: 444)
-			Capybara.app_host = 'https://localhost:444'
+			wait_for_nextcloud(https: true, port: 15136)
+			Capybara.app_host = 'https://localhost:15136'
 
 			visit "/"
-			assert_uri(https: true, port: 444)
+			assert_uri(https: true, port: 15136)
 
 			# Also assert that we can change it back to the default
-			set_config "ports.https": 443
+			set_config "ports.https": DEFAULT_HTTPS_PORT
 			expect($?.to_i).to eq 0
 			wait_for_nextcloud(https: true)
-			Capybara.app_host = 'https://localhost'
+			Capybara.app_host = DEFAULT_HTTPS_HOST
 
 			visit "/"
-			assert_uri(https: true, port: 443)
+			assert_uri(https: true, port: DEFAULT_HTTPS_PORT)
 		end
 
 
 		scenario "http still redirects to unchanged https" do
-			set_config "ports.http": 81
+			set_config "ports.http": 21803
 			expect($?.to_i).to eq 0
-			wait_for_nextcloud(port: 81)
-			Capybara.app_host = 'http://localhost:81'
+			wait_for_nextcloud(port: 21803)
+			Capybara.app_host = 'http://localhost:21803'
 
 			visit "/"
-			assert_uri(https: true, port: 443)
+			assert_uri(https: true, port: DEFAULT_HTTPS_PORT)
 		end
 
 
 		scenario "http redirects to changed https" do
-			set_config "ports.http": 81, "ports.https": 444
+			set_config "ports.http": 21803, "ports.https": 15136
 			expect($?.to_i).to eq 0
-			wait_for_nextcloud(port: 81)
-			Capybara.app_host = 'http://localhost:81'
+			wait_for_nextcloud(port: 21803)
+			Capybara.app_host = 'http://localhost:21803'
 
 			visit "/"
-			assert_uri(https: true, port: 444)
+			assert_uri(https: true, port: 15136)
 		end
 
 		scenario "Let's Encrypt challenge request" do
 			# Assert we do not redirect under the four possibilities for
 			# changing or not changing ports
-			assert_lets_encrypt_challenge(http_port: 80, https_port: 443)
-			assert_lets_encrypt_challenge(http_port: 80, https_port: 444)
-			assert_lets_encrypt_challenge(http_port: 81, https_port: 443)
-			assert_lets_encrypt_challenge(http_port: 81, https_port: 444)
+			assert_lets_encrypt_challenge(http_port: DEFAULT_HTTP_PORT, https_port: DEFAULT_HTTPS_PORT)
+			assert_lets_encrypt_challenge(http_port: DEFAULT_HTTP_PORT, https_port: 15136)
+			assert_lets_encrypt_challenge(http_port: 21803, https_port: DEFAULT_HTTPS_PORT)
+			assert_lets_encrypt_challenge(http_port: 21803, https_port: 15136)
 		end
 	end
 
